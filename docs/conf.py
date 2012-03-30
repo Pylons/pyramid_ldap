175173968fcbcc781eb33eaabb5ee73af87628b8
Chris McDonough <chrism@plope.com>
2012-03-30 06:29:24 -0400

    another hail mary

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 import sys, os
 
-# Add and use Pylons theme
+# Add and use Pyramid theme
 if 'sphinx-build' in ' '.join(sys.argv): # protect against dumb importers
     from subprocess import call, Popen, PIPE
 
@@ -38,17 +38,6 @@ if 'sphinx-build' in ' '.join(sys.argv): # protect against dumb importers
         os.chdir(cwd)
 
     sys.path.append(os.path.abspath('_themes'))
-
-    parent = os.path.dirname(os.path.dirname(__file__))
-    sys.path.append(os.path.abspath(parent))
-    wd = os.getcwd()
-    os.chdir(parent)
-    os.system('%s setup.py test -q' % sys.executable)
-    os.chdir(wd)
-
-    for item in os.listdir(parent):
-        if item.endswith('.egg'):
-            sys.path.append(os.path.join(parent, item))
 
 # General configuration
 # ---------------------
