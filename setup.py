3c0d1603dc08889c1507ee2b40b446b670fd52e1
Steve Piercy <web@stevepiercy.com>
2018-05-02 01:46:44 -0700

    steal code from Pyramid to skip over CHANGES.txt error

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
 
 requires = [
     'pyramid>=1.3a9',
