e62216ed0bfa1af09c26bd7f5076974f76f7c959
Eric Rasmussen <ericrasmussen@gmail.com>
2018-05-02 09:07:37 -0700

    bump pyramid lower bound to safer version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError:
     README = CHANGES = ''
 
 requires = [
-    'pyramid>=1.3a9',
+    'pyramid>=1.3',
     'six', # required by `ldappool` but not in their requirements file
     ]
 if not 'READTHEDOCS' in os.environ:
