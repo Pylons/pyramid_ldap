9543ea895f90bcddb4f12c607dff6d59181f961b
Chris Morales <s3gfault@protonmail.com>
2018-05-15 14:09:27 -0700

    fixed versioning to release with proper tag

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = '2017, Agendaless Consulting <chrism@plope.com>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.3.1'
+version = '0.3.1.post1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
